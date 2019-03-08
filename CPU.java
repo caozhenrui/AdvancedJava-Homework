937007f9dfeda09b406959477eb67950c54e49f9
caozhenrui <48080966+caozhenrui@users.noreply.github.com>
2019-03-08 17:02:54 +0800

    Delete CPU.java

--- CPU.java
+++
@@ -1,9 +0,0 @@
-package lab1;
-
-public abstract class CPU {
-	public void work() {
-		System.out.println("CPU is working");
-	}
-	public abstract int getPrice();
-	public abstract void getDescription();
-}
