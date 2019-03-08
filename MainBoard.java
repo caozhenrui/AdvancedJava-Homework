2929ecf1e5ab1194b95a0cac09d27e08de47fd3c
caozhenrui <48080966+caozhenrui@users.noreply.github.com>
2019-03-08 17:00:59 +0800

    Delete MainBoard.java

--- MainBoard.java
+++
@@ -1,9 +0,0 @@
-package lab1;
-
-public abstract class MainBoard {
-	public void work() {
-		System.out.println("MainBoard is working");
-	}
-	public abstract int getPrice();
-	public abstract void getDescription();
-}
