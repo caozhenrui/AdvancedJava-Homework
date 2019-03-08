f5ecf518497a59435c68b2593825fce38c88e6fe
caozhenrui <48080966+caozhenrui@users.noreply.github.com>
2019-03-08 17:01:40 +0800

    Delete HardDisk.java

--- HardDisk.java
+++
@@ -1,9 +0,0 @@
-package lab1;
-
-public abstract class HardDisk {
-	public void work() {
-		System.out.println("HardDisk is working");
-	}
-	public abstract int getPrice();
-	public abstract void getDescription();
-}
